07e5619481cb155757b1cbf653f65946bcf7fa91
Rhys Rustad-Elliott <me@rhysre.net>
2023-01-10 20:01:31 -0800

    Add GDT setup/flushing code

---
+++ src/include/gdt.h
@@ -0,0 +1,6 @@
+#ifndef __GDT_H__
+#define __GDT_H__
+
+void gdt_init();
+
+#endif /* __GDT_H__ */
---
+++ src/mm/gdt_reload.asm
@@ -0,0 +1,33 @@
+global flush_gdt
+
+; rdi = uint32 code
+; rsi = uint32 data
+bits 64
+flush_gdt:
+    push rbp
+    mov rbp, rsp
+
+    push qword rsi
+    push qword rbp
+
+    pushf
+    pop rax
+    ; Mask TF, NT & RF, all of which have effects on iret we don't want
+    and rax, 0b1111111111111111111111111111111111111111111111101011111011111111
+    push rax
+    popf
+
+    pushf
+    push qword rdi
+    push qword longjmp_target
+    iretq
+
+longjmp_target:
+    mov ds, rsi
+    mov es, rsi
+    mov ss, rsi
+    mov gs, rsi
+    mov fs, rsi
+
+    leave
+    ret
